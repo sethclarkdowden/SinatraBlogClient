8230304c6a78766e937f1fcb03e34ca71546a480
Seth Dowden <sethclarkdowden@gmail.com>
2013-03-07 09:58:51 -0800

    hi

--- blog.rb
+++ blog.rb
@@ -15,6 +15,19 @@ class Post
 	property :auther,		String
 	property :created_at,	DateTime
 	property :updated_at,	DateTime
+
+	has n, :comments
+end
+
+class Comment
+	include DataMapper::Resource
+	
+	property :id,			Serial
+	property :post_id,		Serial
+	property :name,			String
+	property :body,			Text
+
+	belongs_to :post
 end
 
 #Create or upgrade all tables at one 
@@ -32,7 +45,14 @@ get '/' do
 end
 
 get '/post/:id' do
-
+	@post = Post.get(params[:id])
+	@comments = 1
+	@comments = Comment.get(params[:post_id])
+	if @post
+		erb :post
+	else
+		redirect('/')
+	end
 end
 
 get '/new' do 
@@ -42,7 +62,7 @@ end
 
 post '/create' do 
 	@post = Post.new(params[:post])
-	@post
+	@post.save
 	# if @post.save
 	# 	redirect "/post/#{@post.id}"
 	# else
@@ -51,11 +71,25 @@ post '/create' do
 end
 
 get '/delete/:id' do 
-
+	post = Post.get(params[:id])
+	post.destroy
+	redirect('/')
 end
 
 get '/edit/:id' do 
+	@post = Post.get(params[:id])
+	erb :edit
+end
 
+post '/update/:id' do 
+	@post = Post.get(params[:id])
+	@post.update(params[:post])
+	@post.save
+	# if @post.save
+	# 	redirect "/post/#{@post.id}"
+	# else
+	 	redirect('/')
+	# end
 end
 
 
